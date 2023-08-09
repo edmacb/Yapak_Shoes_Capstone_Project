ea82dcc83ce70256781940db362b75e11e9dcad3
Ed <119173327+edmacb@users.noreply.github.com>
2023-08-09 23:25:40 +0800

    added products page, fixed bugs, added dependencies

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const path = require("path");
-const expressLayouts = require("express-ejs-layouts");
-
 const app = express();
+
+const expressLayouts = require("express-ejs-layouts");
+const browserSync = require("browser-sync").create();
+const bsConfig = require("./bs-config.js");
+const { items } = require("./constants/items.js");
+const { sizes } = require("./constants/sizes.js");
+browserSync.init(bsConfig);
+app.use(require("connect-browser-sync")(browserSync));
 
 app.use(expressLayouts);
 app.set("layout", "./layouts/master");
@@ -30,13 +36,13 @@ app.get("/cart", (req, res) => {
   res.render("pages/cart");
 });
 // product page
-app.get('/products', (req, res) => {
+app.get("/products", (req, res) => {
   const viewsData = {
     items: items,
     sizes: sizes,
-  }
-  res.render('pages/products', viewsData)
-})
+  };
+  res.render("pages/products", viewsData);
+});
 
-app.listen(5000);
+app.listen(3000);
 console.log("Server is running");
