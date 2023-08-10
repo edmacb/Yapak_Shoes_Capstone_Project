7682788eb3e2e31b4484329c69e9fcd5f4974d35
Kelotbalut <gabrielle7.ks@gmail.com>
2023-08-10 22:37:50 +0800

    separate product page

--- constants/items.js
+++ constants/items.js
@@ -1,8 +1,9 @@
 const items = [
   {
     imageRef:
-      'https://images.unsplash.com/photo-1494232410401-ad00d5433cfa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+      'https://bristolshoes.ph/cdn/shop/products/Margaret_Black_Duo_370x.progressive.jpg?v=1678087541',
     title: 'Title',
+    gender: "male",
     colors: '8 colors',
     price: '$200',
     description:
@@ -12,6 +13,7 @@ const items = [
     imageRef:
       'https://images.unsplash.com/photo-1587590227264-0ac64ce63ce8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
     title: 'Title 1',
+    gender: "female",
     colors: '8 colors',
     price: '$200',
     description:
@@ -24,6 +26,7 @@ const items = [
     price: '$200',
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    gender: "male"
   },
 ]
 
--- constants/sizes.js
+++ constants/sizes.js
@@ -1,8 +1,13 @@
 const sizes = [
-  { label: 'XS', value: 'xs', id: 'size-0' },
-  { label: 'S', value: 's', id: 'size-1' },
-  { label: 'M', value: 'm', id: 'size-2' },
-  { label: 'L', value: 'l', id: 'size-3' },
+  { label: '8', value: 'xs', id: 'size-0' },
+  { label: '8.5', value: 's', id: 'size-1' },
+  { label: '9', value: 'm', id: 'size-2' },
+  { label: '9.5', value: 'l', id: 'size-3' },
+  { label: '10', value: 'l', id: 'size-3' },
+  { label: '10.5', value: 'l', id: 'size-3' },
+  { label: '11', value: 'l', id: 'size-3' },
+  { label: '11.5', value: 'l', id: 'size-3' },
+  { label: '12', value: 'l', id: 'size-3' },
 ]
 
 module.exports = {
