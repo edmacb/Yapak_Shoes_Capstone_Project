865e8986c505ba8327f7122682b4c9b52e1a60e7
Ed <119173327+edmacb@users.noreply.github.com>
2023-08-12 17:38:02 +0800

    added faq page

--- constants/categories.js
+++ constants/categories.js
@@ -1,10 +1,10 @@
 const categories = [
-  { label: 'Lace-ups', value: 'xs', id: 'size-0' },
-  { label: 'Loafers and Divers', value: 's', id: 'size-1' },
-  { label: 'Boots and Chukkas', value: 'm', id: 'size-2' },
-  { label: 'Mules and Sandals', value: 'l', id: 'size-3' },
-]
+  { label: "Lace-ups", value: "xs", id: "size-0" },
+  { label: "Loafers and Divers", value: "s", id: "size-1" },
+  { label: "Boots and Chukkas", value: "m", id: "size-2" },
+  { label: "Mules and Sandals", value: "l", id: "size-3" },
+];
 
 module.exports = {
   categories,
-}
+};
